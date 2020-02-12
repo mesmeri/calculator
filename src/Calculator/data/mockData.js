0cb0e64b9571d2694512347c58c617e7798d1aa4
OksanaShuptar <angels_ua@ukr.net>
2020-02-12 22:47:03 +0200

    feat: add mock data

---
+++ src/Calculator/data/mockData.js
@@ -0,0 +1,25 @@
+const mockData = [
+  {
+    MSRP: '2200',
+    vehicleName: 'Honda Civic',
+    dealerName: 'Andrei',
+    phone: '0-123-456-678',
+    rating: '5',
+  },
+  {
+    MSRP: '2800',
+    vehicleName: 'Toyota RAV4',
+    dealerName: 'Nicolas',
+    phone: '0-120-009-998',
+    rating: '3',
+  },
+  {
+    MSRP: '3500',
+    vehicleName: 'Acura RDX',
+    dealerName: 'Anna',
+    phone: '0-143-562-913',
+    rating: '4',
+  },
+];
+
+export default mockData;
